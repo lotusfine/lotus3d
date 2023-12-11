ce92d7c3b8c1abefd54f483cb7dc15e1cd30d033
lucas yalet <lucasyalet@lucass-MacBook-Air.local>
2023-12-11 12:57:55 -0300

    cambios hechos desde macbook

--- 3d/main.js
+++ 3d/main.js
@@ -4,22 +4,25 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
+  antialias: true,
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight);
-camera.position.setZ(80);
+camera.position.setZ(30);
 camera.position.setY(10);
 camera.position.setX(20);
 
 renderer.render(scene, camera)
 
-//creacion del planeta
+//creacion del planeta 
 const textura = new THREE.TextureLoader().load('planetal.png');
 
 const planeta = new THREE.Mesh(
@@ -29,11 +32,30 @@ const planeta = new THREE.Mesh(
       displacementMap: textura,
       displacementScale: 0.2,
       bumpMap: textura,
-      
     } )
 );
 
 scene.add(planeta);
+
+// Cargar el mapa de emisión
+const luzNoche = new THREE.TextureLoader().load('luzNoche.png');
+
+//capa luces nocturnas
+const ciudades = new THREE.Mesh(
+    new THREE.SphereGeometry(4.45, 64, 64 ),
+    new THREE.MeshStandardMaterial( {
+       // configuracion luces nocturnas
+       map: luzNoche,
+       displacementMap: luzNoche,
+       displacementScale: 0.2,
+       bumpMap: luzNoche,
+       transparent: true,
+       emissive : new THREE.Color(0x8A2BE2),
+     
+    })
+);
+scene.add(ciudades);
+
 
 //objetos multiples con textura
 const starTexture = new THREE.TextureLoader().load('estrellas.jpg');
@@ -56,7 +78,9 @@ function addStar(){
 
 Array(200).fill().forEach(addStar);
 
-
+//fondo de estrellas
+const fondoEstrellas = new THREE.TextureLoader().load('2k_stars.jpg');
+scene.background = fondoEstrellas;
   
 // Luz Primaria
 const sun = new THREE.DirectionalLight(0xFFA07A, 6, 50);
@@ -108,9 +132,8 @@ const sound = new THREE.PositionalAudio(listener);
 const audioLoader = new THREE.AudioLoader();
 audioLoader.load('space.mp3', function(buffer) {
   sound.setBuffer(buffer);
-  sound.setLoop(true);
+  sound.setLoop(false);
   sound.setVolume(0.2);
-  sound.play();
 });
 
 planeta.add(sound);
@@ -120,41 +143,58 @@ sound.setRefDistance(20); // Distancia a la que el sonido es más fuerte
 sound.setRolloffFactor(1); // Cómo decrece el volumen con la distancia
 
 
-//scroll animation
+//posicion del planeta  
 planeta.position.z = -0.1;
 planeta.position.setX(11);
+ciudades.position.z = -0.1;
+ciudades.position.setX(11);
+
+
+
+//funciones
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Rotación continua del planeta
+  planeta.rotation.y += 0.002;
+  ciudades.rotation.y += 0.002;
+  
+ // Actualizar la intensidad de la emisión basada en la posición del sol y del planeta
+ updateCityLightsIntensity();
+
+ controls.update();
+ renderer.render(scene, camera);
+
+}
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
-  planeta.rotation.x += 0.05;
-  planeta.rotation.y += 0.075;
-  planeta.rotation.z += 0.05;
 
-  // Asegúrate de que estos valores sean lo suficientemente grandes como para notar el cambio
+  // Ajustar la posición de la cámara basada en el scroll
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
   camera.position.y = t * -0.0002;
-  
-  console.log(camera.position.z); // Esto te ayudará a entender si se están aplicando los cambios
 }
 
 document.body.onscroll = moveCamera;
 
+function updateCityLightsIntensity() {
+  const sunDirection = new THREE.Vector3().subVectors(sun.position, planeta.position).normalize();
+  const planetPosition = new THREE.Vector3().copy(planeta.position).normalize();
 
-//animacion
-function animate() {
-  requestAnimationFrame( animate );
+  // Calcular el producto punto
+  const dot = sunDirection.dot(planetPosition);
 
-  planeta.rotation.y += 0.002;
-  planeta.rotation.x += 0.000;
-  planeta.rotation.z += 0.000;
-  
-  //refresco del control de camara
-  controls.update();
-
-
-  renderer.render(scene, camera);
+  // Ajustar la intensidad de las luces de la ciudad
+  // Queremos que la intensidad sea mayor cuando el 'dot' es negativo (parte trasera del planeta)
+  ciudades.material.emissiveIntensity = (dot < 0) ? 3 : -1; 
 }
+
+function onDocumentClick(){
+  sound.play();
+}
+document.addEventListener('click', onDocumentClick);
 
 animate();
 
